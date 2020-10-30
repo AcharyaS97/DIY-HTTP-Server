c1d0efcf3cdcf638b4741d7f5a7070c8323a04c8
Saheel Acharya <saheel97@gmail.com>
2020-10-29 21:37:22 -0400

    Create a ServerSocket listening for max 2 connections on port 6789

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,38 @@
 package com.company;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
 	// write your code here
+        try {
+            createSocket();
+        }
+        catch(Exception e){
+            System.out.println("This is an exception: " + e.getLocalizedMessage());
+        }
+
     }
+
+    public static void createSocket() throws IOException {
+
+        var serverSocket = new ServerSocket(6789,1);
+
+        Socket newConnection = serverSocket.accept();
+
+        System.out.println("Someone has connected!");
+
+        Scanner s = new Scanner(System.in);
+
+        s.nextLine();
+
+
+
+
+    }
+
 }
