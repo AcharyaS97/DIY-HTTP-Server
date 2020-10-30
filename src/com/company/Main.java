bf23aee5eea9256bf6eb0fd04ec40af56a0d0f9d
Saheel Acharya <saheel97@gmail.com>
2020-10-29 22:50:41 -0400

    Add method to read data across client socket connection

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,9 @@
 package com.company;
 
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.Charset;
 import java.util.Scanner;
 
 public class Main {
@@ -11,38 +11,49 @@ public class Main {
     public static void main(String[] args){
 	// write your code here
         try {
-            createAndWriteToSocket();
+
+            var serverSocket = new ServerSocket(6789,1);
+            serverSocket.setReceiveBufferSize(4096);
+
+            Socket newConnection = serverSocket.accept();
+
+            System.out.println("Someone has connected!");
+            System.out.println("Send a message to the client:");
+
+            Scanner scanner = new Scanner(System.in);
+
+            String messageToClient = scanner.nextLine();
+            writeMessageToClient(newConnection,messageToClient);
+
+            readMessageFromClient(newConnection);
+
+            System.out.println("Closing the server");
+            serverSocket.close();
         }
         catch(Exception e){
-            System.out.println("This is an exception: " + e.getLocalizedMessage());
+            System.out.println("This is an exception:" + e.getLocalizedMessage());
         }
 
     }
 
-    public static void createAndWriteToSocket() throws IOException, InterruptedException {
-
-        var serverSocket = new ServerSocket(6789,1);
-        Scanner s = new Scanner(System.in);
-        Socket newConnection = serverSocket.accept();
-
-        System.out.println("Someone has connected!");
-
+    public static void writeMessageToClient(Socket socketConnection,String message) throws IOException {
         //PrintWriter object will write to the client's output stream across the socket connection it has established
-        PrintWriter out = new PrintWriter(newConnection.getOutputStream(),true);
-
-        out.println("I'm sending something back to the client!");
-
-        s.nextLine();
-        int count = 0;
-        while(count != 30){
-            Thread.sleep(2000);
-            out.println(count);
-            count+=1;
-        }
-
-        s.nextLine();
+        PrintWriter out = new PrintWriter(socketConnection.getOutputStream(),true);
+        out.println(message);
     }
 
+    public static void readMessageFromClient(Socket socketConnection) throws IOException {
+
+        InputStream socketInputStream = socketConnection.getInputStream();
+
+        BufferedInputStream bufferedInputStream = new BufferedInputStream(socketInputStream);
+
+        InputStreamReader inputStreamReader = new InputStreamReader(bufferedInputStream);
+
+        BufferedReader reader = new BufferedReader(inputStreamReader,socketConnection.getReceiveBufferSize());
+
+        System.out.println("Client sent a message:" + reader.read());
+    }
 
 
 }
